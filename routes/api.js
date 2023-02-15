790fcd32d7c54ec21980fa28d7a8a399997b0819
IPlayer0neI <101530455+IPlayer0neI@users.noreply.github.com>
2023-02-14 22:22:59 -0300

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,24 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
-
+  
+  app.route("/api/convert")
+    .get(function(req, res){
+       let input = req.query.input;
+       let initNum = convertHandler.getNum(input);
+       let initUnit = convertHandler.getUnit(input);
+       convertHandler.isNumAndUnitValid(initNum, initUnit, (err) => res.send(err), function(){
+        let returnUnit = convertHandler.getReturnUnit(initUnit);
+        let returnNum = convertHandler.convert(initNum, initUnit);
+        let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit, res);
+        let response = {
+          initNum: initNum,
+          initUnit: initUnit,
+          returnUnit: returnUnit,
+          returnNum: returnNum,
+          string: string
+        };
+        res.json(response);
+       });
+    });
 };
